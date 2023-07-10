3840aab6766a49daea5ca0d7b0e6b3abe8b0e34d
Min Chang Joon <mincj93@naver.com>
2023-07-11 00:50:40 +0900

    ㅅㅈ

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { motion, AnimatePresence } from "framer-motion"; // 화면전환 애니메이션
-
-import MainPage from './MainPage';
-import SubPage from './SubPage';
+import React, { useState } from 'react';
+import './App.css';
 
 const App = () => {
 
 	
+	let [글제목, 글제목변경] = useState(['남자 코트 추천' , '강남 우동맛집' , '파이썬독학']);
+	let [따봉, 따봉변경] = useState(0);
+
 	return (
 		<div className='App'>
-			<BrowserRouter>
-				<AnimatePresence>
-				<Routes>
-					<Route path="/" element={<MainPage />}></Route>
-					<Route path="SubPage" element={<SubPage />}></Route>
-				</Routes>
-				</AnimatePresence>
-			</BrowserRouter>
+			<div className='black-nav'>
+				<h4>ReactBlog</h4>
+			</div>
+
+			<button onClick={()=>{
+				글제목[0] = '여자 코트 추천';
+				글제목변경(글제목);
+			}}>글수정</button>
+
+			<div className='list'>
+				<h4>{글제목[0]}<span onClick={() => {따봉변경(따봉+1)}}>👍</span> {따봉} </h4>
+				<p>2월 17일 발행</p>
+			</div>
+			<div className='list'>
+				<h4>{ 글제목[1]}</h4>
+				<p>2월 17일 발행</p>
+			</div>
+			<div className='list'>
+				<h4>{글제목[2]}</h4>
+				<p>2월 17일 발행</p>
+			</div>
 		</div>
 	);
 };
