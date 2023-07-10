4655b0e12278bb5135929f0f8551c6b083c18462
mincj <mincj93@naver.com>
2023-07-10 11:12:39 +0900

    ㅇㅇ

---
+++ src/SubPage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { motion } from "framer-motion";
+
+const SubPage = () => {
+	const routeVariants = {
+        initial: {
+            y: '100vh'
+        },
+        final: {
+            y: '0vh'
+        }
+    }
+
+	return (
+        <motion.div 
+        variants={routeVariants}
+        initial="initial"
+        animate="final"
+        style={{backgroundColor : 'aqua'}}>
+		    <h1><a href='/'>MainPage 이동</a></h1>
+        </motion.div>
+	);
+};
+
+export default SubPage;
