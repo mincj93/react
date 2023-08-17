57259c295dc8e8234fe12a2e87c5851e5e68d2ce
민창준 <mincj93@naver.com>
2023-08-17 16:17:13 +0900

    tt

--- src/test.js
+++ src/test.js
@@ -1,5 +1,6 @@
 const lg = console.log;
 
+
 let arr1 = [1,2,3];
 
 let arr3 = arr1;
@@ -9,3 +10,9 @@ let arr2 = [...arr1];
 lg(arr1 == arr3);
 
 lg(arr1 == arr2);
+
+let arr4 = ['johsy','mincj','love'];
+
+arr4.map((val,i) => {
+    lg(`${i}번째 값은 ${val} 이다`);
+})
