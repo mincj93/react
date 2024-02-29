42fe58d9f0dde190e6cbb7bbe158d6a413c5a701
mincj93 <mincj93@naver.com>
2024-02-29 17:00:41 +0900

    엑셀 작성, 읽기 기능

--- functions/src/App.js
+++ functions/src/App.js
@@ -3,13 +3,19 @@ import { Route, Routes, useLocation, Link } from 'react-router-dom';
 import './App.css';
 
 import DragNdrop from './pages/DragNdrop';
+import ExcelReader from './pages/ExcelReader';
+import ExcelWriter from './pages/ExcelWriter';
 
 function App() {
   return (
     <div className="App">
       <Link to="/dragNdrop">드래그 앤 드랍</Link>
+      <Link to="/excelReader">엑셀데이터 JSON 화</Link>
+      <Link to="/excelWriter">엑셀만들기</Link>
       <Routes>
         <Route path="/dragNdrop" element={<DragNdrop />} />
+        <Route path="/excelReader" element={<ExcelReader />} />
+        <Route path="/excelWriter" element={<ExcelWriter />} />
       </Routes>
 
     </div>
---
+++ functions/src/pages/ExcelReader.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import * as XLSX from 'xlsx';
+// import "./functionCSS/common.css"
+
+const lg = console.log;
+
+const ExcelReader = () => {
+    const [excelData, setExcelData] = useState(null);
+
+    const handleFileUpload = (e) => {
+        const file = e.target.files[0];
+        const reader = new FileReader();
+
+        reader.onload = (event) => {
+            const data = new Uint8Array(event.target.result);
+            const workbook = XLSX.read(data, { type: 'array', encoding: 'utf-8' });
+            const sheetName = workbook.SheetNames[0];
+            const sheet = workbook.Sheets[sheetName];
+            const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+
+            setExcelData(jsonData);
+        };
+
+        reader.readAsArrayBuffer(file);
+    };
+
+    return (
+        <div>
+            <h1>엑셀 읽기</h1>
+            <input type="file" onChange={handleFileUpload} />
+            {excelData && (
+                <div style={{ width: "100%" }}>
+                    <h2>엑셀 데이터</h2>
+                    <pre>{JSON.stringify(excelData, null, 2, 'UTF-8')}</pre>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default ExcelReader;
