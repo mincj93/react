e46470042342dc4cdf59d40147dface38bdd156a
mincj93 <mincj93@naver.com>
2024-03-03 14:45:35 +0900

    팝업 수정

--- functions/src/pages/Popup.js
+++ functions/src/pages/Popup.js
@@ -15,43 +15,40 @@ const Popup = () => {
         document.addEventListener("mousedown", clickOutside);
 
         return () => {
-            // Cleanup the event listener 
-            // 이거 꼭 구현해주자. 상위 객체에서 useState가 값이 유지되어 한 번 더 클릭해야 작동하는 오류를 방지함.
+            // Cleanup the event listener
             document.removeEventListener("mousedown", clickOutside);
         };
     }, [actionModal]);
 
     return (
         <div ref={node}>
-            <div className="flex shrink-0 pointer">
-                <button onClick={() => setActionModal((pre) => !pre)}>모달창 생성</button>
+            <div>
+                <button onClick={() => setActionModal((pre) => !pre)}>모달생성</button>
             </div>
             {actionModal ? (
-                <Modal setActionModal={setActionModal} />
-            )
-                :
-                null}
+                <Modal />
+            ) : null}
         </div>
-    );
+    )
 }
+
 
 const Modal = (props) => {
     return (
         <div
             style={{
-                position: "relative",
+                position: "fixed",
                 backgroundColor: "white",
+                width: "150px",
                 padding: "0.625rem",
                 border: "1px solid grey",
                 zIndex: 10,
                 borderRadius: "4px",
-                backgroundColor: "aqua"
             }}
         >
-            <div className="p-10" style={{ border: "1px solid" }}>
-                <button onClick={() => props.setActionModal((pre) => !pre)}>모달창 닫기</button>
-                모달창 입니다
-            </div>
+            <div className="p-10">action sheet</div>
+            <div className="p-10">action sheet</div>
+            <div className="p-10">action sheet</div>
         </div>
     );
 }
