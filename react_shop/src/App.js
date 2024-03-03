083e199198f2ec97324644262c560d2f26c21380
mincj93 <mincj93@naver.com>
2024-03-03 17:46:45 +0900

    dz

---
+++ react_shop/src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button, Navbar, Container, Nav } from 'react-bootstrap'
+
+function App() {
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
