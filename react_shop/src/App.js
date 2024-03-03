0831c1fc586180537ef2ecfd1b6d5bc4cb85a7e3
mincj93 <mincj93@naver.com>
2024-03-04 00:16:16 +0900

    ff

--- react_shop/src/App.js
+++ react_shop/src/App.js
@@ -1,8 +1,14 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Button, Navbar, Container, Nav } from 'react-bootstrap'
+import data from './data';
+
 
 function App() {
+
+  let [shoes] = useState(data);
+
   return (
     <div>
       <Navbar bg="dark" variant="dark">
@@ -18,26 +24,25 @@ function App() {
 
       <div className="container">
         <div className="row">
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
-            <h4>상품명</h4>
-            <p>상품정보</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
-            <h4>상품명</h4>
-            <p>상품정보</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
-            <h4>상품명</h4>
-            <p>상품정보</p>
-          </div>
+          <Card shoes={shoes[0]} i={1} />
+          <Card shoes={shoes[1]} i={2} />
+          <Card shoes={shoes[2]} i={3} />
         </div>
       </div>
     </div>
 
   );
+}
+
+// 카드 컴포넌트
+function Card(props) {
+  return (
+    <div className="col-md-4">
+      <img src={'https://codingapple1.github.io/shop/shoes' + props.i + '.jpg'} width="80%" />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.price}</p>
+    </div>
+  )
 }
 
 export default App;
