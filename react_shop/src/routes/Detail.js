efa4539a45c6d0cc492cad465d2c2763bd64e952
kionpark <kionpark@gmail.com>
2024-03-19 13:44:22 +0900

    dz

--- react_shop/src/routes/Detail.js
+++ react_shop/src/routes/Detail.js
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import styled from 'styled-components'
 
 const Detail = (props) => {
 
-    let {id} = useParams();
+    let { id } = useParams();
+
+    let [num, setNum] = useState('');
+
+    // styled-components 써보기
+    let Box = styled.div`
+        padding : 20px;
+        background : gray;
+    `;
+
+    let YellowBtn = styled.button`
+        background : yellow;
+        color : black;
+        padding : 10px;
+    `;
+
+    const isNum = (val) => {
+        if (isNaN(val) == true) {
+            alert('그러지마');
+        }
+    }
+
+
+    useEffect(() => {
+        isNum(num);
+    }, [num])
+
 
     return (
         <>
+            <div>
+                <input onChange={(e) => { setNum(e.target.value) }}></input>
+            </div>
             <div className="col-md-6">
                 <img src={"https://codingapple1.github.io/shop/shoes" + props.shoes[id].id + ".jpg"} width="100%" />
             </div>
